f96a4055f6c774696e58481b029d6488c48476b6
robertryanmoore <moore.robert.ryan@gmail.com>
2022-08-31 00:46:45 +0200

    cleaned up css and changed tab icon.  Added more buttons

--- webserver.py
+++ webserver.py
@@ -13,13 +13,15 @@ def hello():
 @app.route('/red')
 def get_red():
     print('red')
-    key = 'w'
+    key = 's'
     keyboard.press(key)
     return render_template('index.html')
 
 @app.route('/blue')
 def get_blue():
     print('blue')
+    key = 'h'
+    keyboard.press(key)
     return render_template('index.html')
 
 @app.route('/yellow')
@@ -29,11 +31,15 @@ def get_yellow():
 
 @app.route('/green')
 def get_green():
+    key = 'a'
+    keyboard.press(key)
     print('green')
     return render_template('index.html')
 
 @app.route('/purple')
 def get_purple():
+    key = 'l'
+    keyboard.press(key)
     print('purple')
     return render_template('index.html')
 
@@ -42,6 +48,19 @@ def get_ashypurple():
     print('ashy purble')
     return render_template('index.html')
 
+@app.route('/orange')
+def get_orange():
+    key = 'o'
+    keyboard.press(key)
+    print('orange')
+    return render_template('index.html')
+
+@app.route('/white')
+def get_white():
+    key = 'm'
+    keyboard.press(key)
+    print('white')
+    return render_template('index.html')
     
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

79f0c1daa67de616621fc91c71e92dd27cbae4be
robertryanmoore <moore.robert.ryan@gmail.com>
2023-06-18 00:24:57 +0200

    fixed keyboard issue

--- lights.py
+++ lights.py
@@ -2,6 +2,7 @@ from time import sleep
 import requests
 from pynput import keyboard
 
+keyboard = keyboard.Controller()
 lastKey = ''
 
 #url = 'http://192.168.100.20:41111/v1/'
@@ -45,7 +46,31 @@ def wontLetGo():
     print()
 
 def undefeated():
-    print()
+    global lastKey 
+    while True:
+        try:
+            sleep(0.5)
+            print("before")
+            slide = requests.get(url+'status/slide?chunked=false')
+            sleep(0.5)
+            if slide.json()['current']['notes'] == 'V1' and lastKey != 'a':
+                keyboard.press('a')
+                keyboard.release('a')
+                lastKey = 'a'
+                print("slide 1")
+            if slide.json()['current']['notes'] == 'B' and lastKey != 'b':
+                keyboard.press('b')
+                keyboard.release('b')
+                lastKey = 'b'
+            if(slide.json()['current']['notes'] == 'C' and lastKey != 'c'):
+                keyboard.press('c')
+                keyboard.release('c')
+                lastKey = 'c'
+
+            sleep(0.1)
+        except Exception as e:
+            print(e)
+            
 
 def singWhereverIgo():
     print()
@@ -63,6 +88,7 @@ def main():
             song = r.json()['presentation']['id']['name'] 
             if(song in 'You Never Stop'):
                 print(song)
+                youNeverStop()
             elif(song in 'Hindsight'):
                 print(song)
             elif(song in 'Sinking Deep'):
@@ -75,6 +101,7 @@ def main():
                 print(song)
             elif(song in 'Undefeated'):
                 print(song)
+                undefeated()
             elif(song in 'Sing Wherever I Go'):
                 print(song)
             elif(song in 'House Of The Lord'):
